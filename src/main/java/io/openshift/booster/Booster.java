e4acd0091eaf682a77f1e3b342e5f882317748ea
seethavi <vijay.seetharaman@gmail.com>
2018-10-09 10:39:04 +1300

    Updated to include a print statement

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,6 @@
 package io.openshift.booster;                                                      
 public class Booster {                                                      
- public static void main(String[] args) { }                                                      
+ public static void main(String[] args) { 
+  System.out.println("Hello World");
+ }                                                      
 } 
